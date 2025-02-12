1c2281edcfd42295330380cf2a5215deb3f2ea22
Mackson Ncube <mightymetrika@proton.me>
2025-02-12 03:33:54 +0000

    add twoTabApp example to unit tests

--- tests/testthat/test-authModule.R
+++ tests/testthat/test-authModule.R
@@ -78,3 +78,135 @@
 #   expect_s3_class(runAuthApp(), "shiny.appobj")
 #
 # })
+
+# testthat::test_that("authModule & postgresModule work together",{
+#
+#   twoTabApp <- function() {
+#     ui <- shiny::fluidPage(
+#       shiny::titlePanel("Two-Tab App: Auth + Random Cars Sampling"),
+#       # A tabsetPanel with an ID
+#       shiny::tabsetPanel(
+#         id = "main_tabs",
+#         # Only the Login tab is initially present
+#         shiny::tabPanel("Login / Signup", value = "auth_tab",
+#                         authUI("auth_module"))
+#       )
+#     )
+#
+#     server <- function(input, output, session) {
+#       # 1) Auth Postgres module + authServer
+#       db_config_auth <- list(
+#         dbname    = Sys.getenv("DBNAME"),
+#         datatable = Sys.getenv("DATATABLEAUTH"),
+#         host      = Sys.getenv("HOST"),
+#         port      = as.integer(Sys.getenv("PORT")),
+#         user      = Sys.getenv("USER"),
+#         password  = Sys.getenv("PASSWORD")
+#       )
+#       postgres_module_auth <- postgresServer(
+#         "postgres_auth",
+#         dbname    = db_config_auth$dbname,
+#         datatable = db_config_auth$datatable,
+#         host      = db_config_auth$host,
+#         port      = db_config_auth$port,
+#         user      = db_config_auth$user,
+#         password  = db_config_auth$password,
+#         data      = shiny::reactive(NULL)
+#       )
+#       auth <- authServer("auth_module", postgres_module_auth, user_table = "users")
+#
+#       # 2) Cars Postgres module
+#       db_config_cars <- list(
+#         dbname    = Sys.getenv("DBNAME"),
+#         datatable = Sys.getenv("DATATABLE"),
+#         host      = Sys.getenv("HOST"),
+#         port      = as.integer(Sys.getenv("PORT")),
+#         user      = Sys.getenv("USER"),
+#         password  = Sys.getenv("PASSWORD")
+#       )
+#       postgres_module_cars <- postgresServer(
+#         "postgres_cars",
+#         dbname    = db_config_cars$dbname,
+#         datatable = db_config_cars$datatable,
+#         host      = db_config_cars$host,
+#         port      = db_config_cars$port,
+#         user      = db_config_cars$user,
+#         password  = db_config_cars$password,
+#         data      = shiny::reactive(NULL)
+#       )
+#
+#       # 3) Define the entire second tab
+#       cars_tab <- shiny::tabPanel(
+#         title = "Cars Random Sampling",
+#         value = "cars_tab",
+#         shiny::sidebarLayout(
+#           shiny::sidebarPanel(
+#             shiny::uiOutput("auth_status_message"),
+#             shiny::numericInput("sample_size", "Number of rows to sample:",
+#                                 value = 5, min = 1, max = nrow(cars)),
+#             shiny::actionButton("sample_button", "Sample Data"),
+#             postgresUI("postgres_cars")$submit,
+#             postgresUI("postgres_cars")$download
+#           ),
+#           shiny::mainPanel(
+#             shiny::uiOutput("sampled_data_title"),
+#             shiny::tableOutput("sampled_data"),
+#             shiny::h4("Database Content:"),
+#             postgresUI("postgres_cars")$table
+#           )
+#         )
+#       )
+#
+#       # 4) Show or remove that second tab based on auth
+#       shiny::observeEvent(auth()$user_auth, {
+#         if (isTRUE(auth()$user_auth)) {
+#           shiny::removeTab(inputId = "main_tabs", target = "cars_tab")
+#           shiny::appendTab(inputId = "main_tabs", cars_tab, select = FALSE)
+#         } else {
+#           shiny::removeTab(inputId = "main_tabs", target = "cars_tab")
+#         }
+#       })
+#
+#       # 5) Logic for sampling, now with username appended
+#       sampled_data <- shiny::reactiveVal(NULL)
+#       shiny::observeEvent(input$sample_button, {
+#         shiny::req(auth()$user_auth)
+#
+#         # Sample from cars
+#         sz <- min(input$sample_size, nrow(cars))
+#         new_sample <- cars[sample(nrow(cars), size = sz, replace = TRUE), ]
+#
+#         # Add the user name as an extra column
+#         new_sample$username <- auth()$info$user
+#
+#         sampled_data(new_sample)
+#
+#         # Pass data to the "cars" module
+#         postgres_module_cars$data_to_submit(new_sample)
+#       })
+#
+#       output$sampled_data_title <- shiny::renderUI({
+#         shiny::req(sampled_data())
+#         shiny::h4("Sampled Data:")
+#       })
+#       output$sampled_data <- shiny::renderTable(sampled_data())
+#
+#       output$auth_status_message <- shiny::renderUI({
+#         if (!isTRUE(auth()$user_auth)) {
+#           shiny::p("You are not logged in. Please sign up, log in, or continue as guest.")
+#         } else {
+#           shiny::p(
+#             shiny::strong("Authenticated as:"),
+#             auth()$info$user,
+#             "(role:", auth()$info$permissions, ")"
+#           )
+#         }
+#       })
+#     }
+#
+#     shiny::shinyApp(ui, server)
+#   }
+#
+#   expect_s3_class(twoTabApp(), "shiny.appobj")
+#
+# })
